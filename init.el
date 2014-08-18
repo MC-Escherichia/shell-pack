a6eb9ae5a3427dc3d1dad09b92cf10e75af4c28d
Matthew Conway <matthew.f.conway@gmail.com>
2014-08-18 14:57:59 -0400

    fix tab in terminal for that zsh goodness

--- init.el
+++ init.el
@@ -16,6 +16,10 @@
 (require 'exec-path-from-shell)
 (exec-path-from-shell-initialize)
 
+(defun term-send-tab ()
+  (interactive)
+  (term-send-raw-string "\t"))
+
 ;; With this snippet, another press of C-d will kill the term buffer.
 (defun comint-delchar-or-eof-or-kill-buffer (arg)
   (interactive "p")
@@ -28,14 +32,36 @@
   (local-set-key (kbd "C-c C-j") 'term-line-mode)
   (smartscan-mode))
 
-(dolist (hook '(sh-mode-hook term-mode-hook shell-mode-hook eshell-mode-hook ))
-  (add-hook hook 'shell-pack/mode-and-simple-bindings-fn))
-
 (defun shell-pack/close-buffer-hook-fn ()
   "Add a binding that permits to kill the buffer when C-d if no input char to kill."
   (local-set-key (kbd "C-d") 'comint-delchar-or-eof-or-kill-buffer))
 
+ (dolist (hook '(sh-mode-hook term-mode-hook shell-mode-hook eshell-mode-hook ))
+  (add-hook hook 'shell-pack/mode-and-simple-bindings-fn))
+
+
+ (defun last-term-buffer (l)
+      "Return most recently used term buffer."
+      (when l
+	(if (eq 'term-mode (with-current-buffer (car l) major-mode))
+	    (car l) (last-term-buffer (cdr l)))))
+
+(defun get-term ()
+      "Switch to the term buffer last used, or create a new one if
+    none exists, or if the current buffer is already a term."
+      (interactive)
+      (let ((b (last-term-buffer (buffer-list))))
+	(if (or (not b) (eq 'term-mode major-mode))
+	    (multi-term)
+	  (switch-to-buffer b))))
+
+
+(defun shell-pack/fix-tab-hook ()
+  (interactive)
+  (local-set-key "\t" 'term-send-tab)
+  (local-set-key (kbd "M-RET") 'get-term))
+
 (dolist (hook '(term-mode-hook shell-mode-hook eshell-mode-hook ))
-  (add-hook hook 'shell-pack/close-buffer-hook-fn))
+  (add-hook hook 'shell-pack/fix-tab-hook))
 
 ;;; shell-pack.el ends here
